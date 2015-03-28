15b9b309eb20759c32f1a3f86da463b6331c839c
lgsds <lgsds@users.noreply.github.com>
2015-03-27 18:19:55 -0700

    Switch to Ratchet and implement basic index
    Bootstrap was not very accomodating for writing a mobile app. Ratchet seems easier to use for implementing a basic mobile app layout. Initial index page with click drop down was implemented as well.

--- js/coop.js
+++ js/coop.js
@@ -1,12 +1,7 @@
-/*jQuery for clicking and sliding*/
-$(document).ready(function(){
-	$("#FoodItems h3").click(function(){
-		//slide up all the link lists
-		$("#FoodItems ul ul").slideUp();
-		//slide down the link list below the h3 clicked - only if its closed
-		if(!$(this).next().is(":visible"))
-		{
-			$(this).next().slideDown();
-		}
-	})
-})
+$(function() {
+
+	// Toggle sub-menu items.
+	$(".table-view-cell").click(function() {
+		$(this).next(".items").children("li").toggle();
+	});
+});
