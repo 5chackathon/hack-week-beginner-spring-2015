737b353fc0796f12b1369cdb4c0c3ae3460107aa
lgsds <lgsds@users.noreply.github.com>
2015-04-04 16:56:43 -0700

    Cleanup code and files

--- js/coop.js
+++ js/coop.js
@@ -1,47 +1,44 @@
 /* Submit an order. */
-function orderClick(item, base_price) {
+var orderClick = function(item, base_price) {
 
-    // Get the checkboxes. 
-    var list = $('.table-view-cell input:checkbox');
-    var options = [];
-    var price = +parseFloat(base_price);
-    var message = "";
+  // Get the checkboxes. 
+  var list = $('.table-view-cell input:checkbox');
+  var options = [];
+  var price = +parseFloat(base_price);
+  var message = "";
 
-    // Save the options and prices of the checked boxes.
-    for (var i = 0; i < list.length; i++) {
-       if ($(list[i]).prop('checked')) {
-            options.push($(list[i]).val());
-            price += parseFloat($(list[i]).data().price);
-       }
+  // Save the options and prices of the checked boxes.
+  for (var i = 0; i < list.length; i++) {
+    if ($(list[i]).prop('checked')) {
+      options.push($(list[i]).val());
+      price += parseFloat($(list[i]).data().price);
+    }
+  }
+
+  // Build up our email message.
+  message += "Your order is a " + item + ((options.length > 0) ? " with " : "");
+
+  // Add options to message.
+  for (var i = 0; i < options.length; i++) {
+    // Add commas if multiple options.
+    if (i < options.length - 1) {
+      message += options[i] + ", ";
+    } else {
+      message += options[i];
     }
 
-    // Build up our email message.
-    message += "Your order is a " + item + ((options.length > 0) ? " with " :"");
-    
-    // Add options to message.
-    for (var i = 0; i < options.length; i++) {
-        // Add commas if multiple options.
-        if (i < options.length - 1) {
-            message += options[i] + ", ";
-        } else {
-            message += options[i];
-        }
-        
-    }
-    message+=". Your total price is $" + price.toFixed(2) + ".";
+  }
+  message += ". Your total price is $" + price.toFixed(2) + ".";
 
-    console.log(message);
-    
-    // Send the order e-mail.
-    //sendMail(message);
+  console.log(message);
+
+  // Send the order e-mail.
+  //sendMail(message);
 }
 
 /* Send an e-mail with the order information. */
 function sendMail(message) {
-    var link = "mailto:me@example.com?"
-             + "&subject=" + encodeURIComponent("Coop Fountain Order")
-             + "&body=" + encodeURIComponent(message);
-    ;
+  var link = "mailto:me@example.com?" + "&subject=" + encodeURIComponent("Coop Fountain Order") + "&body=" + encodeURIComponent(message);;
 
-    window.location.href = link;
+  window.location.href = link;
 }
