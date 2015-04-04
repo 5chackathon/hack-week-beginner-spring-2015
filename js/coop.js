fa837cd05cd8acc43a05b10fabb69a7aa91d182a
Kevin4theNguyen <kan02012@mymail.pomona.edu>
2015-04-03 23:27:54 -0700

    Implement add to tray and basic e-mail template.

--- js/coop.js
+++ js/coop.js
@@ -1,10 +1,33 @@
-$(function() {
+function orderClick(base_price) {
+    
+//    alert("hello");
 
+    var list = $('.table-view-cell input:checkbox');
+    var options = [];
+    var price = parseFloat(base_price);
+    console.log(list.length);
+    for (var i = 0; i < list.length; i++) {
+       if ($(list[i]).prop('checked')) {
+            options.push($(list[i]).val());
+            price += parseFloat($(list[i]).data().price);
+       }
+    }
+    console.log(price);
+    console.log(options);
 
-	// Toggle sub-menu items.
-	// $(".table-view-cell").click(function() {
-	// 	$(this).next(".items").children("li").toggle();
-	// });
+//	 Toggle sub-menu items.
+	 // alert($('input[name="bagel"]:checked').length)
+     
 
 	
-});
+}
+
+function sendMail() {
+    var link = "mailto:me@example.com"
+             + "?cc=myCCaddress@example.com"
+             + "&subject=" + escape("This is my subject")
+             + "&body=";
+    ;
+
+    window.location.href = link;
+}
