9e1b61f74c5b8218500f85c177bb6c786509b92c
lgsds <lgsds@users.noreply.github.com>
2015-04-04 10:56:40 -0700

    Build e-mail message with order options
    Script parses user inputs and builds a message that will be sent in an e-mail through the user's mail applicaiton.

--- js/coop.js
+++ js/coop.js
@@ -1,32 +1,46 @@
-function orderClick(base_price) {
-    
-//    alert("hello");
+/* Submit an order. */
+function orderClick(item, base_price) {
 
+    // Get the checkboxes. 
     var list = $('.table-view-cell input:checkbox');
     var options = [];
-    var price = parseFloat(base_price);
-    console.log(list.length);
+    var price = +parseFloat(base_price);
+    var message = "";
+
+    // Save the options and prices of the checked boxes.
     for (var i = 0; i < list.length; i++) {
        if ($(list[i]).prop('checked')) {
             options.push($(list[i]).val());
             price += parseFloat($(list[i]).data().price);
        }
     }
-    console.log(price);
-    console.log(options);
 
-//	 Toggle sub-menu items.
-	 // alert($('input[name="bagel"]:checked').length)
-     
+    // Build up our email message.
+    message += "Your order is a " + item + ((options.length > 0) ? " with " :"");
+    
+    // Add options to message.
+    for (var i = 0; i < options.length; i++) {
+        // Add commas if multiple options.
+        if (i < options.length - 1) {
+            message += options[i] + ", ";
+        } else {
+            message += options[i];
+        }
+        
+    }
+    message+=". Your total price is $" + price.toFixed(2) + ".";
 
-	
+    console.log(message);
+    
+    // Send the order e-mail.
+    //sendMail(message);
 }
 
-function sendMail() {
-    var link = "mailto:me@example.com"
-             + "?cc=myCCaddress@example.com"
-             + "&subject=" + escape("This is my subject")
-             + "&body=";
+/* Send an e-mail with the order information. */
+function sendMail(message) {
+    var link = "mailto:me@example.com?"
+             + "&subject=" + encodeURIComponent("Coop Fountain Order")
+             + "&body=" + encodeURIComponent(message);
     ;
 
     window.location.href = link;
